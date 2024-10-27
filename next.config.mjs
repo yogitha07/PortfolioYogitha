3f2b0250bc579f60d7fa581f33bf853e1d3ef667
yogitha07 <vijaya.07yogitha.99@gmail.com>
2024-10-27 10:30:51 +0530

    Commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
     images: {
       unoptimized: true, // Disables image optimization for GitHub Pages
     },
-    basePath: '/your-repo-name', // Replace 'your-repo-name' with your GitHub repository name
-    assetPrefix: '/your-repo-name/', // Ensures assets load correctly on GitHub Pages
+    basePath: '/PortfolioYogitha', // Replace 'your-repo-name' with your GitHub repository name
+    assetPrefix: '/PortfolioYogitha/', // Ensures assets load correctly on GitHub Pages
   };
   
   export default nextConfig;
